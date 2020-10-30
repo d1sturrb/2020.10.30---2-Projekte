3e60a51bc1a15dd95d06d3c0a1c4de5b28f06c62
d1sturrb <alexander.biber@outlook.com>
2020-10-30 15:07:33 +0100

    yes

--- Alex - Fetching the weather/fetch_local_weather.py
+++ Alex - Fetching the weather/fetch_local_weather.py
@@ -6,23 +6,25 @@ from haversine import haversine
 stations = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getallstations'
 weather = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/'
 
-my_latitude = 47.624219
-my_longitude = 7.669055
+#latitude, longitude
+home = [47.624219, 7.669055]
+#my_latitude = 47.624219
+#my_longitude = 7.669055
 
 all_stations = get(stations).json()["items"]
 
-def find_closest():
+def find_closest(chosen_longitude, chosen_latitude):
     smallest = 20036
     for station in all_stations:
         station_lon = station['weather_stn_long']
         station_lat = station['weather_stn_lat']
-        distance = haversine(my_longitude, my_latitude, station_lon, station_lat)
+        distance = haversine(chosen_longitude, chosen_latitude, station_lon, station_lat)
         if distance < smallest:
             smallest = distance
             closest_station = station['weather_stn_id']
     return closest_station
 
-closest_station_id = find_closest()
+closest_station_id = find_closest(home[0], home[1])
 
 weather += str(closest_station_id)
 
